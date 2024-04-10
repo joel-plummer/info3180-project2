90aab84be8075751d145e30f465c267bc1bc29c2
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-04-10 00:49:16 -0500

    Setup Database and Create Models

--- app/models.py
+++ app/models.py
@@ -1,1 +1,41 @@
-# Add any model classes for Flask-SQLAlchemy here
+from . import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    location = db.Column(db.String(50))
+    biography = db.Column(db.String(300))
+    profile_photo = db.Column(db.String(100))
+    joined_on = db.Column(db.DateTime, nullable=False)
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String(200), nullable=False)
+    photo = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False)
+
+
+class Like(db.Model):
+    __tablename__ = "likes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+
+class Follow(db.Model):
+    __tablename__ = "follows"
+
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
