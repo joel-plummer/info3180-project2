ca9a436af5ca11ee98420c4a21402874b2c31d59
Michael Leighton <michael138leighton@gmail.com>
2024-04-12 15:31:36 -0500

    Add foreign keys and relationships to the db

--- app/models.py
+++ app/models.py
@@ -10,32 +10,41 @@ class User(db.Model):
     lastname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     location = db.Column(db.String(50))
-    biography = db.Column(db.String(300))
+    biography = db.Column(db.String(255))
     profile_photo = db.Column(db.String(100))
-    joined_on = db.Column(db.DateTime, nullable=False)
+    joined_on = db.Column(db.DateTime, default=db.func.current_timestamp())
+    
+    #relationships
+    osts = db.relationship('Post', backref='user', lazy=True)
+    likes = db.relationship('Like', backref='user', lazy=True)
+    followers = db.relationship('Follow', foreign_keys='Follow.follower_id', backref='follower', lazy='dynamic')
+    follows = db.relationship('Follow', foreign_keys='Follow.user_id', backref='following', lazy='dynamic')
 
 
 class Post(db.Model):
     __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True)
-    caption = db.Column(db.String(200), nullable=False)
-    photo = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
-    created_on = db.Column(db.DateTime, nullable=False)
+    caption = db.Column(db.String(255), nullable=False)
+    photo = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_on = db.Column(db.DateTime, default=db.func.current_timestamp())
+    
+    #relationship
+    likes = db.relationship('Like', backref='post', lazy=True)
 
 
 class Like(db.Model):
     __tablename__ = "likes"
 
     id = db.Column(db.Integer, primary_key=True)
-    post_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
 class Follow(db.Model):
     __tablename__ = "follows"
 
     id = db.Column(db.Integer, primary_key=True)
-    follower_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, index=True)
