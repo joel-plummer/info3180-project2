838957fe062ad7bf5a69cb4012f83eb1f19af6e8
Aaliyah-Reid <aaliyahreid12345@gmail.com>
2024-04-26 18:10:43 -0500

    explore page

---
+++ src/views/ExploreView.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="explore-right">
+            <RouterLink  class="link" to="/posts/new">New Post</RouterLink>
+        </div>
+</template>
+
+
+<style>
+
+.explore-right {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+ }
+
+ .explore-right .link {
+    text-decoration: none;
+    color: #fff;
+    background: rgb(36, 154, 184);
+    height: 30px;
+    width: 150px;
+    text-align: center;
+    border-radius: 3px;
+    align-items: center;
+ }
+</style>
